d6b739f5e41e997833c1c5b0ca72cb152db9d8ee
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-09 18:01:41 +0530

    Chat Container implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -53,7 +53,7 @@ export const sendMessage = async (req, res) => {
       image: imageUrl,
     });
 
-    newMessage.save();
+    await newMessage.save();
 
     res.status(201).json(newMessage);
   } catch (error) {
